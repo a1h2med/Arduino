e086f56a01b2f8f9dc44f1733304dd633c6fa97a
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-02-10 23:19:18 +0200

    Add files via upload

---
+++ temprature_sensor/temprature_sensor.ino
@@ -0,0 +1,15 @@
+int tempPin=A0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+     int reading=analogRead(tempPin);
+     float voltage=reading*5;
+     voltage/=1024;
+     float Temprature_in_C=(voltage-.5)*100*-1;
+     Serial.println(Temprature_in_C);
+     delay(1000);
+}

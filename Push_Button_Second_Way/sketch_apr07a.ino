bd4d2370e76acc120e076328a86a8d28fc66b1cf
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-02-10 23:55:45 +0200

    Add files via upload

---
+++ Push_Button_Second_Way/sketch_apr07a.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode (8,OUTPUT);
+pinMode (7, INPUT);
+}
+
+int state = 0;
+int times = 0;
+
+void loop() {
+state = digitalRead(7);
+if(state == 1){
+  times = times + 1;
+}
+if(times%2 == 0){
+  digitalWrite(8,LOW);
+}else{
+  digitalWrite(8,HIGH);
+}
+}
---
+++ RC_car_code/RC_car_code.ino
@@ -0,0 +1,45 @@
+int echo1=13,trig1=12;
+int echo2=7,trig2=8;
+int motor1[]={10,11},motor2[]={9,6},motor3[]={2,4},servo[]={5,3};
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(echo1,OUTPUT);
+pinMode(echo2,OUTPUT);
+pinMode(trig1,INPUT);
+pinMode(trig2,INPUT);
+for (int i=0;i<2;i++)
+{
+  pinMode(motor1[i],OUTPUT);
+  pinMode(motor2[i],OUTPUT);
+  pinMode(motor3[i],OUTPUT);
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(trig1,HIGH);
+digitalWrite(trig2,HIGH);
+delayMicroseconds(1000);
+digitalWrite(trig1,LOW);
+digitalWrite(trig2,LOW);
+int take=pulseIn(echo1,HIGH);
+int donno=pulseIn(echo2,HIGH);
+int distance=take/2;
+int space=donno/2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

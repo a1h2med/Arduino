26277de6493423f32f6ea895604b9c2c3faef768
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-02-10 23:20:13 +0200

    Add files via upload

---
+++ PID_with_POT/Task_3.ino
@@ -0,0 +1,20 @@
+#include <PID_v1.h>
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+//Specify the links and initial tuning parameters
+PID myPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
+void setup()
+{
+ //initialize the variables we're linked to
+ Setpoint = 10;
+ //turn the PID on
+ myPID.SetMode(AUTOMATIC);
+ Serial.begin(9600);
+}
+void loop()
+{
+ Input = analogRead(A0);
+myPID.Compute();
+ analogWrite(3,Output);
+ Serial.println(Output);
+}

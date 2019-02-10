919231b49ea929b6f400f6fb3628b48ef84f1279
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-02-10 23:27:35 +0200

    Add files via upload

---
+++ Moving_TX&RX/sketch_jul13a.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(11,10); //RX, TX
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(9,OUTPUT);
+digitalWrite(9,HIGH);
+Serial.begin(9600);
+BTSerial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(BTSerial.available()){
+    Serial.write(BTSerial.read());
+  }
+}

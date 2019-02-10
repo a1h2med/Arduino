eec1aa8e274c459d510f883e54707eee375eeb33
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-02-10 23:25:55 +0200

    Add files via upload

---
+++ UltraSonic/sketch_jul30a.ino
@@ -0,0 +1,29 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9,OUTPUT);
+  pinMode(10,INPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+Serial.begin(9600);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(9,HIGH);
+delayMicroseconds(1000);
+digitalWrite(9,LOW);
+int duration=pulseIn(10,HIGH);
+int distance=duration/2;
+Serial.println(distance);
+if (distance>=200 && distance<=500)
+{
+  digitalWrite(7,HIGH);
+  digitalWrite(8,LOW);
+}
+else if (distance<200)
+{
+   digitalWrite(8,HIGH);
+  digitalWrite(7,LOW);
+}
+delay(1000);
+}

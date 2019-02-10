b0f6e8a0888c27dfcd17db0926a85c65a3e8eee7
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-02-10 23:25:07 +0200

    Add files via upload

---
+++ Sound_Sensor/sketch_jun29a.ino
@@ -0,0 +1,12 @@
+byte SoundSensor= 8;float value=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(SoundSensor,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+value = analogRead (A0);
+Serial.println(value);
+}

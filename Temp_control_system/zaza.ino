fa089ea9cf2fb3648c17eb7901f1e78e55d32e9a
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2019-05-21 14:01:23 +0200

    Add files via upload

---
+++ Temp_control_system/zaza.ino
@@ -0,0 +1,18 @@
+float x = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode (13,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+x = analogRead(A0);
+x = x * .48828125;
+Serial.println(x);
+if (x > 40)
+  digitalWrite(13,HIGH);
+else if (x < 40)
+  digitalWrite(13,LOW);
+delay (1000);
+} 
